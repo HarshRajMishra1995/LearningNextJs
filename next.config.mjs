59ad5d28369058055812cea9634b1c2226baf301
Harshrajmishra <Harshrajmishra.1995@gmail.com>
2024-06-30 15:38:28 +0530

    completed the dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = { experimental: {
-    ppr: 'incremental',
+  ppr: 'incremental',
+  appDir: true,
   },};
 
 export default nextConfig;
